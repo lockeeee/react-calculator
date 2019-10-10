386ea6f76461a3f8f22e4f02c8e57a40380a185c
lockeeee <mlocek.dawid@gmail.com>
2019-10-10 17:30:57 +0200

    Updates

---
+++ precache-manifest.19e1efc89d609eaaefc560e018c37bdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9151e5e228f0c2a911d50a7801bd557d",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "6567daa69ad8c3d44ea2",
+    "url": "/react-calculator/static/css/main.eecf8414.chunk.css"
+  },
+  {
+    "revision": "784aee1f0b6e384f95e0",
+    "url": "/react-calculator/static/js/2.c4b90b7c.chunk.js"
+  },
+  {
+    "revision": "6567daa69ad8c3d44ea2",
+    "url": "/react-calculator/static/js/main.37eb31f4.chunk.js"
+  },
+  {
+    "revision": "5675be794e1c9004920d",
+    "url": "/react-calculator/static/js/runtime-main.ddcb3a20.js"
+  }
+]);
